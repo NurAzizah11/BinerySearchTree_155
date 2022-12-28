92215bd7ab62bf33cee0b5522a0db77ef325081c
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-28 17:08:58 +0700

    membuat class Node

--- BinerySearchTree_155/Program.cs
+++ BinerySearchTree_155/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace BinerySearchTree_155
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        // Constructor for the Node Cllass
+        
+    }
     internal class Program
     {
         static void Main(string[] args)
