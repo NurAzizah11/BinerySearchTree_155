869c9bafc39149e3413b09211a9df2cb870e6860
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-12-28 17:22:29 +0700

    menambahkan public void search

--- BinerySearchTree_155/Program.cs
+++ BinerySearchTree_155/Program.cs
@@ -32,6 +32,21 @@ namespace BinerySearchTree_155
         {
             ROOT = null; /* Initializing ROOT to null */
         }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /* This function searchs the currentNode of the specified Node as well
+              as the current Node of its parents */
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
         
         }
     }
